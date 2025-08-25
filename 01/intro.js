58fd561244a13b1585a47ef46c4d0dce5e5db6c4
Sanjila Gangaju <canzilagangaju@gmail.com>
2025-08-25 07:59:28 +0545

    first intro to typescript

---
+++ 01/intro.js
@@ -0,0 +1,3 @@
+var user = { name: "sanjila", age: 10 };
+console.log("Sanjila");
+console.log(user.email);
---
+++ 01/intro.ts
@@ -0,0 +1,3 @@
+let user={name:"sanjila", age:10}
+console.log("Sanjila")
+console.log(user.email);
